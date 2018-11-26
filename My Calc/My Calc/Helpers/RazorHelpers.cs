21763a1d8075e86b2ffa9c819ac25ed89482043d
Irina <blossomingflowers8@gmail.com>
2018-11-26 16:31:50 +0300

    final task

---
+++ My Calc/My Calc/Helpers/RazorHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace My_Calc.Helpers
+{
+    public static class RazorHelpers
+    {
+        public static string DisplayName(this Enum value)
+        {
+            Type enumType = value.GetType();
+            var enumValue = Enum.GetName(enumType, value);
+            MemberInfo member = enumType.GetMember(enumValue)[0];
+
+            var attrs = member.GetCustomAttributes(typeof(DisplayAttribute), false);
+            var outString = ((DisplayAttribute)attrs[0]).Name;
+
+            if (((DisplayAttribute)attrs[0]).ResourceType != null)
+            {
+                outString = ((DisplayAttribute)attrs[0]).GetName();
+            }
+
+            return outString;
+        }
+    }
+}
