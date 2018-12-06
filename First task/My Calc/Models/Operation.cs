313239a09f56107fccddeede6e275269fad77031
Irina <blossomingflowers8@gmail.com>
2018-12-06 10:21:51 +0300

    StyleCop 100% ok

---
+++ First task/My Calc/Models/Operation.cs
@@ -0,0 +1,20 @@
+﻿using My_Calc.Resources;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace My_Calc.Models
+{
+    public enum Operation
+    {
+        [Display(Name = "Add", ResourceType = typeof(CalcResources))]
+        Add,
+        [Display(Name = "Residual", ResourceType = typeof(CalcResources))]
+        Residual,
+        [Display(Name = "Multiplication", ResourceType = typeof(CalcResources))]
+        Multiplication,
+        [Display(Name = "NoOrdinaryOperation", ResourceType = typeof(CalcResources))]
+        NoOrdinaryOperation,
+        [Display(Name = "Division", ResourceType = typeof(CalcResources))]
+        Division
+    }
+}
