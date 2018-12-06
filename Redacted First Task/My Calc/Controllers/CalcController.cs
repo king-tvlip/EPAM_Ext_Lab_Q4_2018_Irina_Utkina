9cbb67ae71b6433c62f163b0c4374aa244b680e8
Irina <blossomingflowers8@gmail.com>
2018-12-06 10:47:16 +0300

    Redacted the .csproj file

---
+++ Redacted First Task/My Calc/Controllers/CalcController.cs
@@ -0,0 +1,87 @@
+﻿using My_Calc.Helpers;
+using My_Calc.Models;
+using My_Calc.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace My_Calc.Controllers
+{
+    public class CalcController : Controller
+    {
+        const int DefaultX = 3;
+        const int DefaultY = 32;
+        const string DefaultResult = "No data";
+
+        public static List<string> Results = new List<string>();
+
+        // GET: Calc
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Obsolete]
+        public ActionResult OldAdd(int x, int y)
+        {
+            return View(x + y);
+        }
+
+        /// <summary>
+        /// Calculate the sum of two integer variables
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Add()
+        {
+            return View(new CalcModel() { X = DefaultX, Y = DefaultY, Result = DefaultResult });
+        }
+
+        /// <summary>
+        /// Calculate the sum of two integer variables
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Add(CalcModel model)//не сумма
+
+        {
+            int result = 0;
+            switch (model.Op)
+            {
+                case Operation.Add:
+                    result = model.X + model.Y;
+                    break;
+                case Operation.Multiplication:
+                    result = model.X * model.Y;
+                    break;
+                case Operation.Residual:
+                    result = model.X - model.Y;
+                    break;
+                case Operation.NoOrdinaryOperation:
+                    result = 4 * model.X - 3 * model.Y;
+                    break;
+                case Operation.Division:
+
+                    if (model.Y == 0)
+                    {
+                        Results.Add("Error! Division by zero is incorrect!");
+                        break;
+                    }
+                    else
+                    {
+                        result = model.X / model.Y;
+                        break;
+                    }
+            }
+
+                        model.Result = string.Format("{0}{2}{1} = {3}\n", CalcResources.x, CalcResources.y, model.Op.DisplayName(), result);
+                        Results.Add(model.Result);
+                        Results.Add("The time is" + " " + DateTime.UtcNow);
+                        return View(model);
+                    
+         
+        }
+    }
+}
---
+++ Redacted First Task/My Calc/Models/CalcModel.cs
@@ -0,0 +1,26 @@
+﻿using My_Calc.Resources;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace My_Calc.Models
+{
+    public class CalcModel
+    {
+        [Display(Name = "x", ResourceType = typeof(CalcResources))]
+        [Required]
+        public int X { get; set; }
+
+        [Display(Name = "y", ResourceType = typeof(CalcResources))]
+        [Required]
+        public int Y { get; set; }
+
+        [Display(Name = "Result", ResourceType = typeof(CalcResources))]
+        public string Result { get; set; }
+
+        [Display(Name = "OpName", ResourceType = typeof(CalcResources))]
+        public Operation Op { get; set; }
+    }
+}
