0431fac5b73f05720b2d8c7f51a6a96a88030f89
Irina <blossomingflowers8@gmail.com>
2018-12-20 10:26:29 +0300

    redacted task 6_1

--- Task6_1/Task6/Program.cs
+++ Task6_1/Task6/Program.cs
@@ -1,58 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task6
+﻿namespace Task6
 {
-    class strings_to_compare
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Strings_to_compare
     {
         public delegate List<string> Comparison(List<string> stroka);
         public List<string> Compare_strings(List<string> strlist)
         {
             ////возвращает длину строки
+
             for (int i = 0; i < 2; i++)
             {
-                if (strlist[i+1].Length < strlist[i].Length)
+                if (strlist[i + 1].Length < strlist[i].Length)
                 {
                     var newstring = strlist[i];
                     strlist[i] = strlist[i + 1];
                     strlist[i + 1] = newstring;
                 }
             }
+
             return strlist;
         }
-        //метод сортировки
+
+        ////метод сортировки
+
         public List<string> Order_strings(List<string> strlist)
         {
             ////экземпляр делегата
-            Comparison compare = new Comparison(Compare_strings);
+
+            Comparison compare = new Comparison(this.Compare_strings);
             strlist = compare(strlist);
             return strlist;
         }
 
-        public void clear(List<string> strlist)
+        public void Clear(List<string> strlist)
         {
             strlist.Clear();
         }
 
-        public void show_array(List<string> strlist)
+        public void Show_array(List<string> strlist)
         {
-            foreach(string sentence in strlist)
+            foreach (string sentence in strlist)
             {
                 Console.WriteLine(sentence);
             }
         }
     }
-    class Program
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            strings_to_compare stringe = new strings_to_compare();
+            Strings_to_compare stringe = new Strings_to_compare();
             var strlist = new List<string>() { "first line", "second line", "third line" };
             stringe.Order_strings(strlist);
-            stringe.show_array(strlist);
+            stringe.Show_array(strlist);
             Console.ReadKey();
         }
     }
