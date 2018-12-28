57e0037b376f2401c1900b536acaefb986a17df9
Irina <blossomingflowers8@gmail.com>
2018-12-28 11:51:52 +0300

    Исправила ошибки, оставила комментарии Коли.

---
+++ Fifth task — копия/ClassLibrary1/CommonBtwManagerUser.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassLibrary1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public abstract class CommonBtwManagerUser
+    {
+        public string Name { get; set; }
+
+        public int ID { get; set; }
+    }
+}
