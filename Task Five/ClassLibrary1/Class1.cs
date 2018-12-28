356594084527bccd34110bde94a1bfc7259cd6ad
Irina <blossomingflowers8@gmail.com>
2018-12-28 11:54:30 +0300

    Оставила комментарии Коли, исправила ошибки.

---
+++ Task Five/ClassLibrary1/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Class1
+    {
+    }
+}
---
+++ Task Five/ClassLibrary1/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace ClassLibrary1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public interface IBaseRepository<DefT> where DefT : class, new()
+    {
+        DefT Get(int id);
+
+        List<DefT> GetAll();
+
+        bool Save(DefT entity);
+
+        bool Delete(int id);
+    }
+}
