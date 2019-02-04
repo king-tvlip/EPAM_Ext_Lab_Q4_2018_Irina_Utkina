af10a66f1250f1d76eb10bc095f920e52399dfc7
Irina <blossomingflowers8@gmail.com>
2019-02-04 19:38:38 +0300

    Fixed mistakes

---
+++ Task 6/Task6/Person.cs
@@ -0,0 +1,112 @@
+﻿namespace Task6._2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Person
+    {
+        public Person(string name)
+        {
+            this.PersonName = name;
+        }
+
+        public Person(string name, int arrTime)
+        {
+            this.PersonName = name;
+            this.ArrivalTime = arrTime;
+        }
+
+        public delegate void Message(string Name, Queue<Person> PersonQueue);
+
+        public delegate void GreetMessage(string Name, int time, Queue<Person> PersonQueue);
+
+        public event Message Came;
+
+        public event Message Left;
+
+        public int ArrivalTime { get; set; }
+
+        public string PersonName { get; set; }
+
+        public void OnCame(string name, Queue<Person> personQueue)
+        {
+            if (this.Came != null)
+            {
+                Console.WriteLine(name + " came to work.");
+            }
+        }
+
+        public void OnLeave(string name, Queue<Person> personQueue)
+        {
+            if (this.Left != null)
+            {
+                Person personAway = personQueue.Dequeue();
+                Console.WriteLine(personAway.PersonName + " left office.");
+            }
+        }
+
+        public void ShowMessage(string message, Queue<Person> personQueue)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void CameToWork(string name, Queue<Person> personQueue)
+        {
+            Person person = new Person(name);
+            GreetMessage greetingByPerson = new GreetMessage(person.Greet);
+            Message sayByeByPerson = new Message(person.SayBye);
+            person.Came += this.ShowMessage;
+            person.Left += this.ShowMessage;
+            person.OnCame(name, personQueue);
+
+            person.Greet(name, person.ArrivalTime, personQueue);
+            personQueue.Enqueue(person);
+        }
+
+        public void LeftWork(string name, Queue<Person> personQueue)
+        {
+            Person person = new Person(name);
+            Message sayByeByPerson = new Message(person.SayBye);
+            person.Left += this.ShowMessage;
+            person.OnLeave(name, personQueue);
+            person.SayBye(name, personQueue);
+            /*PersonQueue.Dequeue();*/
+        }
+
+        public void Greet(string name, int time, Queue<Person> personQueue)
+        {
+            if (time < 12)
+            {
+                foreach (Person person in personQueue)
+                {
+                    Console.WriteLine("Good morning" + "," + " " + name + " said " + person.PersonName);
+                }
+            }
+            else if ((time > 12) && (time < 17))
+            {
+                foreach (Person person in personQueue)
+                {
+                    Console.WriteLine("Good morning" + "," + " " + name + " said " + person.PersonName);
+                }
+            }
+            else if (time >= 17)
+            {
+                foreach (Person person in personQueue)
+                {
+                    Console.WriteLine("Good morning" + "," + " " + name + " said " + person.PersonName);
+                }
+            }
+        }
+
+        public void SayBye(string name, Queue<Person> personQueue)
+        {
+            foreach (Person person in personQueue)
+            {
+                Console.WriteLine("Bye!" + name + " said " + person.PersonName);
+            }
+        }
+    }
+}
